a582d2737dcdcce3f8e368959f73599172b2e15a
Mehul Banga <62008222+stfumehul@users.noreply.github.com>
2020-10-04 06:11:20 +0530

    Add files via upload

---
+++ static/graphs.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+          $(chart_id).highcharts({
+              chart: chart,
+              title: title,
+              xAxis: xAxis,
+              yAxis: yAxis,
+              series: series,
+              subtitle:subtitle,
+
+              legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 100,
+        y: 70,
+        floating: true,
+        backgroundColor: Highcharts.defaultOptions.chart.backgroundColor,
+        borderWidth: 1
+    },
+    plotOptions: {
+    scatter: {
+      marker: {
+        radius: 5,
+        states: {
+          hover: {
+            enabled: true,
+            lineColor: 'rgb(100,100,100)'
+          }
+        }
+      },
+      states: {
+        hover: {
+          marker: {
+            enabled: false
+          }
+        }
+      },
+      tooltip: {
+        headerFormat: '<b>{series.name}</b><br>',
+        pointFormat: '{point.x} cm, {point.y} kg'
+      }
+    }
+  }
+          });
+});
